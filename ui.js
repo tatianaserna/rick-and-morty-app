4b82f1d12a184f0007c871cb922fac118c4408b3
tatianaserna <88945242+tatianaserna@users.noreply.github.com>
2025-06-14 15:14:49 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -2,12 +2,15 @@
 function renderCharacters(characters) {
   const container = document.getElementById('character-list');
   container.innerHTML = characters.map(character => `
-    <div class="col-md-4 mb-4">
-      <div class="card text-dark">
+    <div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="card h-100 shadow">
         <img src="${character.image}" class="card-img-top" alt="${character.name}">
         <div class="card-body">
           <h5 class="card-title">${character.name}</h5>
-          <p class="card-text">Estado: ${character.status}</p>
+          <p class="card-text">
+            <strong>Estado:</strong> ${character.status}<br>
+            <strong>Especie:</strong> ${character.species}
+          </p>
         </div>
       </div>
     </div>
